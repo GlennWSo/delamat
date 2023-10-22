195f2cf14589ebdafc8c89a4e9ce51881050008e
GlennWSo <sondellglenn@gmail.com>
2023-10-22 13:19:17 +0200

    auth poc

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use axum::{
     http::{header, StatusCode},
     response::{Html, IntoResponse, Redirect, Response},
     routing::{delete, get, post},
-    Router,
+    Extension, Router,
 };
 use axum_flash::{self, Flash, IncomingFlashes, Key, Level};
 use email_address::{self, EmailAddress};
@@ -14,7 +14,14 @@ use futures_util::stream;
 use log::error;
 use maud::{html, Markup};
 use serde::Deserialize;
+use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
 use terminal_link::Link;
+
+use axum_login::{
+    axum_sessions::{async_session::MemoryStore, SessionLayer},
+    secrecy::SecretVec,
+    AuthLayer, AuthUser, MySqlStore, RequireAuthorizationLayer,
+};
 
 use std::{io, str::FromStr};
 
@@ -293,6 +300,28 @@ impl FromRef<AppState> for axum_flash::Config {
     }
 }
 
+#[derive(Debug, Default, Clone, sqlx::FromRow)]
+struct User {
+    id: i32,
+    password_hash: String,
+    name: String,
+}
+
+impl AuthUser<i32> for User {
+    fn get_id(&self) -> i32 {
+        self.id
+    }
+
+    fn get_password_hash(&self) -> axum_login::secrecy::SecretVec<u8> {
+        let hash = dbg!(self.password_hash.clone());
+        SecretVec::new(hash.into())
+    }
+}
+
+type AuthCtx = axum_login::extractors::AuthContext<i32, User, MySqlStore<User>>;
+
+const DB_URL: &'static str = env!("DATABASE_URL");
+
 #[tokio::main]
 async fn main() {
     let db = DB::new(5).await;
@@ -302,7 +331,58 @@ async fn main() {
         flash_config: axum_flash::Config::new(Key::generate()),
     };
 
+    let secret = [10u8; 64];
+
+    let session_store = MemoryStore::new();
+    let session_layer = SessionLayer::new(session_store, &secret);
+
+    let pool = MySqlPoolOptions::new()
+        .max_connections(2)
+        .connect(DB_URL)
+        .await
+        .unwrap();
+    let user_store = MySqlStore::<User>::new(pool);
+    let auth_layer = AuthLayer::new(user_store, &secret);
+
+    async fn logout_handler(mut auth: AuthCtx) -> String {
+        let msg = dbg!(format!("Logging out user:{:#?}", auth.current_user));
+        auth.logout().await;
+        msg
+    }
+    async fn login_handler(mut auth: AuthCtx) -> String {
+        println!("logging in");
+        let pool = MySqlPoolOptions::new()
+            .max_connections(2)
+            .connect(DB_URL)
+            .await
+            .unwrap();
+        let mut conn = pool.acquire().await.unwrap();
+        let user: User = sqlx::query_as!(User, "select * from users where id = 1")
+            .fetch_one(&mut conn)
+            .await
+            .unwrap();
+        let login_res = auth.login(&user).await;
+        match login_res {
+            Ok(_) => dbg!("logged in".into()),
+            Err(e) => dbg!(format!("failed with: {e}")),
+        }
+    }
+
+    async fn protected_info(Extension(user): Extension<User>) -> String {
+        dbg!(format!("logged in as {:#?}", user))
+    }
+
+    async fn derp() -> String {
+        "derp".to_string()
+    }
+
     let app = Router::new()
+        .route("/pro", get(protected_info))
+        .route_layer(RequireAuthorizationLayer::<i32, User>::login())
+        .route("/logout", get(logout_handler))
+        .route("/login", get(login_handler))
+        .layer(auth_layer)
+        .layer(session_layer)
         .route("/", get(index))
         .route("/contacts", get(home))
         .route("/contacts/download", get(download_archive))
