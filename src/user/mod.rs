33e50bef0487d4deed89815cb78503b582b7250b
GlennWSo <sondellglenn@gmail.com>
2023-10-29 10:32:39 +0100

    validate post new user

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -2,33 +2,35 @@ use std::fmt::Display;
 
 use axum::{
     extract::{Query, State},
-    response::IntoResponse,
+    response::{IntoResponse, Redirect},
     routing::{get, post},
     Extension, Form, Router,
 };
-use axum_flash::{IncomingFlashes, Level};
+use axum_flash::{Flash, IncomingFlashes, Level};
 use axum_login::{
     axum_sessions::{async_session::MemoryStore, SessionLayer},
     secrecy::SecretVec,
     AuthLayer, AuthUser, MySqlStore, RequireAuthorizationLayer,
 };
-use log::error;
+use log::{error, log};
 use maud::html;
 use serde::Deserialize;
+use sqlx::mysql::MySqlQueryResult;
 // use html_macro::html;
 
 mod templates;
 
 use crate::{
-    email::{validate_email, validate_user_email, EmailQuery},
+    db::DB,
+    email::{validate_email, validate_user_email, EmailError, EmailFeedBack, EmailQuery},
     AppState,
 };
 
-use self::templates::new_user_template;
+use self::templates::new_template;
 use crate::templates::Markup;
 
 async fn email_validation(State(state): State<AppState>, Query(q): Query<EmailQuery>) -> Markup {
-    let db_res = validate_user_email(&state.db, q).await;
+    let db_res = validate_user_email(&state.db, &q.email, q.id).await;
     match db_res {
         Ok(email_feedback) => email_feedback.into(),
         Err(db_error) => {
@@ -48,7 +50,7 @@ enum PasswordFormatError {
     NoNumeric,
 }
 
-// const SPECIAL_CHARS: &str = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+const SPECIAL_CHARS: &str = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 impl Display for PasswordFormatError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -68,14 +70,35 @@ struct PasswordQuery {
     password: Box<str>,
 }
 
+impl From<&str> for PasswordQuery {
+    fn from(value: &str) -> Self {
+        Self {
+            password: value.into(),
+        }
+    }
+}
+
 impl PasswordQuery {
     const MIN: u8 = 6;
     const MAX: u8 = 64;
 
+    fn validate_char(c: &char) -> bool {
+        if c.is_alphabetic() {
+            return true;
+        }
+        if c.is_numeric() {
+            return true;
+        }
+        if SPECIAL_CHARS.contains([*c]) {
+            return true;
+        }
+        false
+    }
+
     fn validate(&self) -> Result<(), PasswordFormatError> {
         use PasswordFormatError as Error;
 
-        if let Some(c) = self.password.chars().find(|c| !c.is_ascii()) {
+        if let Some(c) = self.password.chars().find(|c| !Self::validate_char(c)) {
             return Err(Error::NotAscii(c));
         };
 
@@ -111,7 +134,7 @@ async fn validate_password_query(Query(q): Query<PasswordQuery>) -> Markup {
 }
 
 async fn get_new_user(flashes: IncomingFlashes) -> (IncomingFlashes, Markup) {
-    let body = new_user_template(flashes.iter(), None, None);
+    let body = new_template(flashes.iter(), None, None);
     (flashes, body)
 }
 
@@ -121,18 +144,86 @@ struct NewUserInput {
     password: String,
     email: String,
 }
+impl NewUserInput {
+    async fn insert(self, db: &DB) -> sqlx::Result<MySqlQueryResult> {
+        let salt = "badsalt";
+        let hash = "badhash";
+        sqlx::query!(
+            "
+            insert into users (password_hash, name, email, salt)
+            values (?, ?, ?, ?)
+            ",
+            hash,
+            self.name,
+            self.email,
+            salt
+        )
+        .execute(db.conn())
+        .await
+    }
+}
+
+enum NewUserError {
+    PasswordError(PasswordFormatError),
+    EmailError(EmailError),
+    DBError(sqlx::Error),
+}
+impl Display for NewUserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            NewUserError::PasswordError(e) => write!(f, "{e}"),
+            NewUserError::EmailError(e) => write!(f, "{e}"),
+            NewUserError::DBError(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+impl NewUserInput {
+    async fn validate(&self, db: &DB) -> Result<(), NewUserError> {
+        use NewUserError as E;
+
+        let feedback = validate_user_email(db, &self.email, None).await;
+        match feedback {
+            Ok(v) => match v.0 {
+                Ok(_) => (),
+                Err(e) => return Err(E::EmailError(e)),
+            },
+            Err(e) => return Err(E::DBError(e)),
+        };
+
+        let q: PasswordQuery = self.password.as_str().into();
+        match q.validate() {
+            Ok(_) => (),
+            Err(e) => return Err(E::PasswordError(e)),
+        };
+
+        Ok(())
+    }
+}
 
 async fn post_new_user(
     State(state): State<AppState>,
-    // _flash: Flash,
+    flash: Flash,
     Form(input): Form<NewUserInput>,
 ) -> impl IntoResponse {
-    let msg = (
-        Level::Debug,
-        format!("Not yet implemtented: anyways got input:{:#?}", input),
-    );
-    let feedback = validate_email(EmailQuery::new(input.email), &state.db);
-    new_user_template(Some(msg), Some("TODO!"), Some("TODO"))
+    use NewUserError as E;
+    match input.validate(&state.db).await {
+        Ok(_) => {
+            let res = input.insert(&state.db).await;
+            match res {
+                Ok(v) => {
+                    log::info!("created new user, details: {:#?}", v);
+                    (flash.success("created new user"), Redirect::to("/user/new")).into_response()
+                }
+                Err(e) => new_template([(Level::Debug, dbg!(e))], None, None).into_response(),
+            }
+        }
+        Err(e) => match e {
+            E::PasswordError(e) => new_template(None, None, Some(e)).into_response(),
+            E::EmailError(e) => new_template(None, Some(e), None).into_response(),
+            E::DBError(e) => new_template([(Level::Debug, dbg!(e))], None, None).into_response(),
+        },
+    }
 }
 
 #[derive(Debug, Default, Clone, sqlx::FromRow)]
@@ -141,6 +232,7 @@ struct User {
     password_hash: String,
     name: String,
     email: String,
+    salt: Vec<u8>, // [u8; 16]
 }
 
 impl AuthUser<i32> for User {
